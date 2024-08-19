b156dd105381370d89fb741a64d1b04e27a4e3ff
Jaimi Klein jaimiblankenship@gmail.com <jaimiblankenship@gmail.com>
2024-08-19 14:18:59 +0200

    pushing what i have

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    @answer = stupid_coach(@question)
+  end
 
+  private
+
+  def stupid_coach(str)
+
+  end
+
+  def is_question?(str)
+    (str === nil) ? false : str.end_with?("?")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "questions#ask"
+  get "/ask", to: "questions#ask"
   get "/answer", to: "questions#answer"
 end
